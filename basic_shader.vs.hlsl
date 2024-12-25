722b45ada75f15e24ef3f51d464b9b804c5c2496
Roy-Fokker <11281115+Roy-Fokker@users.noreply.github.com>
2024-12-25 17:28:13 -0500

    update shader to use uniform buffer

--- basic_shader.vs.hlsl
+++ basic_shader.vs.hlsl
@@ -4,12 +4,19 @@ struct VSOUTPUT
 	float4 color : COLOR0;
 };
 
+struct Projection
+{
+	float4x4 data;
+};
+
+ConstantBuffer<Projection> projection : register(b0, space0);
+
 VSOUTPUT main(uint vertex_id : SV_VertexID)
 {
 	float3 tri_pos[3] = {
-		{0.5f, -0.5f, 0.f},
-		{-0.5f, -0.5f, 0.f},
-		{0.f, 0.5f, 0.f},
+		{0.5f, -0.5f, 1.5f},
+		{-0.5f, -0.5f, 1.5f},
+		{0.f, 0.5f, 1.5f},
 	};
 
 	float4 tri_col[3] = {
@@ -18,8 +25,11 @@ VSOUTPUT main(uint vertex_id : SV_VertexID)
 		{0.f, 0.f, 1.f, 1.f},
 	};
 
+	float4 pos = float4(tri_pos[vertex_id], 1.f);
+	pos = mul(projection.data, pos);
+
 	VSOUTPUT output = {
-		float4(tri_pos[vertex_id], 1.f),
+		pos,
 		tri_col[vertex_id]
 	};
 	
